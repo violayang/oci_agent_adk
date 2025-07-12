be7e3a552b2e1c29b6e049a1b6c9cb4b28c695b6
aojah1 <aojah1@yahoo.com>
2025-07-12 16:51:30 -0400

    Enabled multi turn conversation

--- src/agents/getinsights.py
+++ src/agents/getinsights.py
@@ -5,8 +5,9 @@ from oci.addons.adk import Agent, AgentClient
 from oci.addons.adk.mcp import MCPClientStreamableHttp
 from pathlib import Path
 from dotenv import load_dotenv
-from src.tools.custom_function_tools import AccountToolkit
 from oci.addons.adk.tool.prebuilt import AgenticRagTool
+from mcp.client.stdio import StdioServerParameters
+from oci.addons.adk.mcp import MCPClientStdio
 
 # ────────────────────────────────────────────────────────
 # 1) bootstrap paths + env + llm
@@ -23,17 +24,22 @@ AGENT_EP_ID = os.getenv("AGENT_EP_ID")
 AGENT_REGION = os.getenv("AGENT_REGION")
 
 
-async def agent_flow(input_message:str):
+async def agent_flow():
 
-    params = StreamableHttpParameters(
-        url="https://cf95-104-48-173-98.ngrok-free.app/mcp",
+    redis_server_params = StreamableHttpParameters(
+        url="https://d7e385f08598.ngrok-free.app/mcp",
     )
 
-    async with MCPClientStreamableHttp(
-        params=params,
-        name="redis mcp server",
-    ) as mcp_client:
+    # Use uvx to run the time MCP server
+    time_server_params = StdioServerParameters(
+        command="uvx",
+        args=["mcp-server-time", "--local-timezone=America/Los_Angeles"],
+    )
 
+    async with (
+        MCPClientStdio(params=time_server_params) as time_mcp_client,
+        MCPClientStreamableHttp(params=redis_server_params) as redis_mcp_client
+    ):
         client = AgentClient(
             auth_type="api_key",
             config=OCI_CONFIG_FILE,
@@ -45,21 +51,18 @@ async def agent_flow(input_message:str):
             client=client,
             agent_endpoint_id=AGENT_EP_ID,
             instructions="You are a Redis-savvy assistant.Only allow read operation with the best tool you have",
-            tools=[await mcp_client.as_toolkit()],
+            tools=[await time_mcp_client.as_toolkit(), await redis_mcp_client.as_toolkit()],
         )
 
         agent.setup()
 
         # Should trigger the `add` tool
+        input_message = "What is the local time now? which Invoice I should pay first based criteria such as highest amount due and highest past due date for 'session:e5f6a932-6123-4a04-98e9-6b829904d27f'"
+
         print(f"Running: {input_message}")
         response = await agent.run_async(input_message)
         response.pretty_print()
-        return agent
 
-async def agent_setup():
-
-        input_message = "which Invoice I should pay first based criteria such as highest amount due and highest past due date for 'session:e5f6a932-6123-4a04-98e9-6b829904d27f'"
-        agent = await agent_flow(input_message)
 
 if __name__ == "__main__":
-    asyncio.run(agent_setup())
+    asyncio.run(agent_flow())
